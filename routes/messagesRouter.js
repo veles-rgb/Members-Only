fa1e6c8f83610c3f96d77d63adbd22fa238120ef
MacMini <181009546+veles-rgb@users.noreply.github.com>
2025-12-15 18:37:51 -0500

    Create /messages/delete route

--- routes/messagesRouter.js
+++ routes/messagesRouter.js
@@ -7,6 +7,7 @@ const validateNewMessage = require("../validators/validateNewMessage");
 router.get("/new", messagesController.renderMessageForm);
 router.post("/new", validateNewMessage, messagesController.postNewMessage);
 router.get("/:id", messagesController.renderMessageId);
+router.post("/delete", messagesController.postDeleteMessage);
 
 
 module.exports = router;
