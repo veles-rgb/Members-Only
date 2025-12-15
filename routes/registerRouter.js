d665dc0b878a73672326013948d0b16988335da3
MacMini <181009546+veles-rgb@users.noreply.github.com>
2025-12-15 14:45:35 -0500

    Add validateRegister to register post route.

--- routes/registerRouter.js
+++ routes/registerRouter.js
@@ -2,8 +2,9 @@ const { Router } = require("express");
 const router = Router();
 
 const registerController = require("../controllers/registerController");
+const validateRegister = require("../validators/validateRegister");
 
 router.get("/", registerController.renderRegisterForm);
-router.post("/", registerController.postRegisterForm);
+router.post("/", validateRegister, registerController.postRegisterForm);
 
 module.exports = router;
