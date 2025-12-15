f23d5d0d89453019af2111c57b8984197d6c1e34
MacMini <181009546+veles-rgb@users.noreply.github.com>
2025-12-15 17:36:33 -0500

    Add validation, postNewMessage
    Added validation, created postNewMessage controllers
    and required logged in user to postNewMessage and renderMessageForm.

--- controllers/messagesController.js
+++ controllers/messagesController.js
@@ -1,6 +1,9 @@
 const db = require("../db/queries");
+const { validationResult } = require("express-validator");
 
 async function renderMessageId(req, res, next) {
+    if (!req.user) return res.redirect("/login");
+
     const id = req.params.id;
     if (!/^\d+$/.test(id)) {
         return res.status(404).render("404", { title: "Page Not Found" });
@@ -22,14 +25,34 @@ async function renderMessageId(req, res, next) {
 }
 
 function renderMessageForm(req, res) {
+    if (!req.user) return res.redirect("/login");
 
     res.render("messageForm", {
-        title: "New Message"
+        title: "New Message",
+        errors: []
     });
 }
 
-async function postNewMessage(req, res) {
+async function postNewMessage(req, res, next) {
+    if (!req.user) return res.redirect("/login");
 
+    const errors = validationResult(req);
+
+    if (!errors.isEmpty()) {
+        return res.status(400).render("messageForm", {
+            title: "New Message",
+            errors: errors.array()
+        });
+    }
+
+    try {
+        const { title, message } = req.body;
+
+        await db.postNewMessage(req.user.id, title, message);
+        res.redirect("/");
+    } catch (error) {
+        return next(error);
+    }
 }
 
 module.exports = {
