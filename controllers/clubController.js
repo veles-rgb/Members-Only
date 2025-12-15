a438127a64b01b88626ef13123fc971f7fb3a68c
MacMini <181009546+veles-rgb@users.noreply.github.com>
2025-12-15 17:39:55 -0500

    Create postClubForm controller, to join club

--- controllers/clubController.js
+++ controllers/clubController.js
@@ -1,14 +1,28 @@
 const db = require("../db/queries");
 
 function renderClubForm(req, res) {
+    if (!req.user) return res.redirect("/login");
 
     res.render("club", {
-        title: "Join The Club!"
+        title: "Join The Club!",
     });
 }
 
-async function postClubForm(req, res) {
+async function postClubForm(req, res, next) {
+    try {
+        if (!req.user) return res.redirect("/login");
 
+        const code = req.body.clubcode;
+
+        if (code !== process.env.CLUB_CODE) {
+            return res.redirect("/club");
+        }
+
+        await db.postJoinClub(req.user.id);
+        return res.redirect("/club");
+    } catch (error) {
+        return next(error);
+    }
 }
 
 module.exports = {
