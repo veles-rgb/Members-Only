e1172ef7099de469f899cb90472a7fac50222917
MacMini <181009546+veles-rgb@users.noreply.github.com>
2025-12-15 14:45:07 -0500

    Create post registerForm route

--- controllers/registerController.js
+++ controllers/registerController.js
@@ -1,15 +1,35 @@
 const db = require("../db/queries");
+const bcrypt = require("bcryptjs");
+const { validationResult } = require("express-validator");
 
 function renderRegisterForm(req, res) {
-
     res.render("registerForm", {
-        title: "Register Account"
+        title: "Register Account",
+        errors: []
     });
 }
 
-async function postRegisterForm(req, res) {
+async function postRegisterForm(req, res, next) {
+    const errors = validationResult(req);
 
+    const { fullname, username } = req.body;
+
+    if (!errors.isEmpty()) {
+        return res.status(400).render("registerForm", {
+            title: "Register Account",
+            errors: errors.array()
+        });
+    }
+
+    try {
+        const hashedPassword = await bcrypt.hash(req.body.password, 10);
+        await db.postRegister(fullname, username, hashedPassword);
+        return res.redirect("/login");
+    } catch (error) {
+        return next(error);
+    }
 }
+
 
 module.exports = {
     renderRegisterForm,
